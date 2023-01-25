2ebc2f57c84f9780aec71caf913abc8d125370cc
Valentin Himself <valentinhimself@gmail.com>
2023-01-25 09:55:12 -0700

    Complete the project

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,23 @@
 export function setContactDOM() {
   const h1 = document.createElement('h1');
-  h1.textContent = 'La Panaderia Contact';
+  const hr = document.createElement('hr');
+  const hr2 = document.createElement('hr');
+  const p = document.createElement('p');
+  const i = document.createElement('p');
+
+  i.classList.add('cursive');
+  i.textContent = 'Bienvenidos';
+  h1.textContent = 'NOSOTROS';
+  h1.classList.add('menu__h1');
   const main = document.querySelector('main');
-  main.append(h1);
+  const div = document.createElement('div');
+  const container = document.createElement('div');
+  container.classList.add('container__div', 'contact-page');
+
+  p.textContent =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque placerat, tellus sit amet ultrices porttitor, ex ex aliquet purus, pellentesque rhoncus felis ipsum nec purus. Donec id enim in risus pellentesque blandit. Proin sed nibh nec arcu mollis rhoncus in sit amet augue. Suspendisse magna nisl, eleifend non tempor et, luctus id quam. Nulla bibendum vulputate ante, non malesuada eros lobortis euismod';
+
+  main.append(div);
+  div.append(container);
+  container.append(i, h1, hr, hr2, p);
 }
